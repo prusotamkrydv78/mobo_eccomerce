1505849ba597843d1266345010c0c1f44ee6adc9
Prusotam <prusotamkumaryadav78@gmail.com>
2025-12-13 11:42:32 +0545

    feat: Implement user address and wishlist management API endpoints and their corresponding logic.

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { addAddress, addToWishlist, deleteAddress, getAddresses, getWishlist, removeFromWishlist, updateAddress } from "../controllers/user.controller";
+import { protectRoute } from "../middlewares/auth";
+const UserRouter = Router();
+
+UserRouter.use(protectRoute)
+
+//Adress Routes
+UserRouter.post("/addresses", addAddress)
+UserRouter.get("/addresses", getAddresses)
+UserRouter.put("/addresses/:addressId", updateAddress)
+UserRouter.delete("/addresses/:addressId", deleteAddress)
+
+//Wishlist routes
+UserRouter.post("/wishlist", addToWishlist)
+UserRouter.get("/wishlist", getWishlist)
+UserRouter.delete("/wishlist/:productId", removeFromWishlist)
+
+export default UserRouter
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,25 @@
 import express from "express";
 import dbConnect from "./config/dbConnect.js";
-import ENV  from "./config/env.js";
+import ENV from "./config/env.js";
 import { clerkMiddleware } from '@clerk/express'
-import {serve} from "inngest/express";
-import {functions,inngest} from "./config/inngest.js";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 import adminRouter from "./routes/admin.route.js";
+import userRouter from "./routes/user.route.js";
 const app = express();
 dbConnect();
 
 
- app.use(express.json())
+app.use(express.json())
 app.use(clerkMiddleware())
-app.use('/api/inngest',serve({client:inngest,functions,signingKey: process.env.INNGEST_SIGN_KEY,}))
+app.use('/api/inngest', serve({ client: inngest, functions, signingKey: process.env.INNGEST_SIGN_KEY, }))
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
 
 app.listen(ENV.PORT, () => {
     console.log(`Server is running on port ${ENV.PORT}`);
