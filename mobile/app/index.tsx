0c1878e9a7747e4352712e0600b600cbf8d35987
Prusotam <prusotamkumaryadav78@gmail.com>
2025-12-17 18:52:28 +0545

    native wind and clerk auth implemented

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,16 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello World At React Native</Text>
+      <Text>This app is developed by Prusotam</Text>
+      <Text>Thanks for using it </Text>
+      <Text>We will love to see you again ❤️❤️❤️</Text>
+      <Button title="Click me" />
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+      </View>
     </View>
   );
 }
